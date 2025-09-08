92d5b239fb326bbdf3863c8d199be6a7b45e8a08
unknown <ghoshprince999@gmail.com>
2025-09-08 13:21:04 +0600

    task_manager_app

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,14 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
-
-
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(body: Center(child: Text("Hello World!"))),
-    );
+    return MaterialApp();
   }
 }

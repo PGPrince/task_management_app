49d0fe2c50c0ef54b2759799758885f13673895c
unknown <ghoshprince999@gmail.com>
2025-09-09 12:36:18 +0600

    home_screen

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,20 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Color(0xffFFFFFF),
         appBar: AppBar(
-          backgroundColor: Color(0xff0BA6DF)
+          backgroundColor: Color(0xff0BA6DF),
+          leading: Icon(
+            Icons.menu_sharp, color: 
+            Color(0xffFFFFFF)
+            ),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 20),
+              child: Icon(
+                Icons.account_circle, 
+                color: Color(0xffFFFFFF),
+                ),
+            ),
+          ],
         ),
         body: SafeArea(
           child: Column(
@@ -26,6 +39,7 @@ class MyApp extends StatelessWidget {
                   'Hello Prashanta!',
                   style: TextStyle(
                     fontSize: 40.17,
+                    fontFamily: 'poppins-bold'
                   ),
                 ),
               ),
@@ -37,7 +51,7 @@ class MyApp extends StatelessWidget {
                 child: Text(
                   'Have a nice day',
                   style: TextStyle(
-                    // fontFamily: 
+                    fontFamily: 'poppins-medium'
                   ),
                 ),
               ),
@@ -107,6 +121,62 @@ class MyApp extends StatelessWidget {
               ),
               SizedBox(
                 height: 20,
+              ),
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20),
+                    child: Container(
+                      height: 300,
+                      width: 300,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Color(0xff9112BC),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                          Image(
+                            image: AssetImage('assets/images/project-management.png'),
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            'Project 1',
+                            style: TextStyle(
+                              fontSize: 22.42,
+                              fontFamily: 'poppins-regular',
+                              color: Color(0xffFFFFFF),
+                            ),
+                          ),
+                        ],
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Text(
+                            'Fond-End\nDevelopment',
+                            style: TextStyle(
+                              fontSize: 30,
+                              fontFamily: 'poppins-medium',
+                              color: Color(0xffFFFFFF),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Text(
+                            'October 20, 2025',
+                            style: TextStyle(),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
